7ac35e016b019309c937ceab2f3fe5d3a98fb43e
Jeff Rooks <onx2rj@gmail.com>
2024-05-14 00:45:24 -0400

    add IMC to player controller
    test with debug message

--- Source/TreasureTide/Private/Player/TidePlayerController.cpp
+++ Source/TreasureTide/Private/Player/TidePlayerController.cpp
@@ -3,3 +3,47 @@
 
 #include "Player/TidePlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+ATidePlayerController::ATidePlayerController()
+{
+	bReplicates = true;
+	bShowMouseCursor = false;
+}
+
+void ATidePlayerController::BeginPlay()
+{
+	SetInputMode(FInputModeGameOnly());
+	Super::BeginPlay();
+
+	checkf(TideContext, TEXT("TidePlayerController requires an Input Mapping Context. Please check the BP for this member."));
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(TideContext, 0);
+	}
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Input
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Input
+void ATidePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	// Bind input actions to functions
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATidePlayerController::MoveTriggered);
+}
+
+void ATidePlayerController::MoveTriggered(const FInputActionValue& ActionValue)
+{
+	APawn* ControlledPawn = GetPawn();
+	if (!IsValid(ControlledPawn)) return;
+
+	const FVector2D InputVector = ActionValue.Get<FVector2D>();
+	GEngine->AddOnScreenDebugMessage(1, 1.0f, FColor::Blue, InputVector.ToString());
+}
+#pragma endregion 
--- Source/TreasureTide/Public/Player/TidePlayerController.h
+++ Source/TreasureTide/Public/Player/TidePlayerController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "TidePlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -13,5 +17,26 @@ UCLASS()
 class TREASURETIDE_API ATidePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ATidePlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Input
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Input
+protected:
+	virtual void SetupInputComponent() override;
 	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> TideContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	void MoveTriggered(const FInputActionValue& ActionValue);
+	
+#pragma endregion 
 };
