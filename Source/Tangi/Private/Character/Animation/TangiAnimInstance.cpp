4bb085b9d5754d0bb5dccfdb46255b625b98b9f1
Jeff Rooks <onx2rj@gmail.com>
2024-05-28 11:04:28 -0400

    save

--- Source/Tangi/Private/Character/Animation/TangiAnimInstance.cpp
+++ Source/Tangi/Private/Character/Animation/TangiAnimInstance.cpp
@@ -3,6 +3,10 @@
 
 #include "Character/Animation/TangiAnimInstance.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PhysicsVolume.h"
+
 void UTangiAnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
@@ -16,4 +20,91 @@ void UTangiAnimInstance::NativeThreadSafeUpdateAnimation(const float DeltaTime)
 	if (!IsValid(Character)) return;
 	
 	CharacterTrajectory = Character->CharacterTrajectory;
+
+	RefreshStance();
+	RefreshSwimming();
+	RefreshVelocity();
+	RefreshFalling();
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Velocity
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Velocity
+void UTangiAnimInstance::RefreshVelocity()
+{
+	PreviousVelocityXY = VelocityXY;
+	VelocityXY = FVector(Character->GetCharacterMovement()->Velocity.X, Character->GetCharacterMovement()->Velocity.Y, 0.f);
+}
+#pragma endregion
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Falling
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Falling
+void UTangiAnimInstance::RefreshFalling()
+{
+	bPreviousFalling = bFalling;
+	bFalling = Character->GetCharacterMovement()->IsFalling() && Character->GetCharacterMovement()->Velocity.Z < UE_SMALL_NUMBER;
+	
+	if (bFalling && !bPreviousFalling) // Character just started falling
+	{
+		FallStart = GetWorld()->GetTimeSeconds();
+		FallDuration = 0.f;
+	}
+	else if (bFalling && bPreviousFalling) // Character is continuing to fall
+	{
+		FallDuration = GetWorld()->GetTimeSeconds() - FallStart;
+	}
+	else // Character is not falling
+	{
+		FallStart = 0.f;
+		FallDuration = 0.f;
+	}
+}
+#pragma endregion
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Stance (Postures on Land)
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Stance
+void UTangiAnimInstance::RefreshStance()
+{
+	PreviousStance = Stance;
+
+	if (Character->bIsCrouched)
+	{
+		Stance = CrouchingStance;
+	}
+	else
+	{
+		Stance = StandingStance;
+	}
+}
+#pragma endregion
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Swimming
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Swimming
+void UTangiAnimInstance::RefreshSwimming()
+{
+	bPreviousSwimming = bSwimming;
+	bPreviousUnderwater = bUnderwater;
+
+	const APhysicsVolume* CurrentVolume = Character->GetPhysicsVolume();
+	const bool bIsInWater = CurrentVolume->bWaterVolume;
+	bSwimming = bIsInWater && Character->GetCharacterMovement()->IsSwimming();
+	if (bSwimming)
+	{
+		const FVector CapsuleTop = Character->GetCapsuleComponent()->GetComponentLocation() + FVector(0.0f, 0.0f, Character->GetCapsuleComponent()->GetScaledCapsuleHalfHeight());
+		const float WaterSurfaceZ = CurrentVolume->GetActorLocation().Z + CurrentVolume->GetBounds().BoxExtent.Z;
+
+		bUnderwater = CapsuleTop.Z <= WaterSurfaceZ;
+	}
+	else
+	{
+		bUnderwater = 0;
+	}
+}
+#pragma endregion 
--- Source/Tangi/Public/Character/Animation/TangiAnimInstance.h
+++ Source/Tangi/Public/Character/Animation/TangiAnimInstance.h
@@ -7,6 +7,13 @@
 #include "Character/TangiCharacterBase.h"
 #include "TangiAnimInstance.generated.h"
 
+
+UENUM(BlueprintType)
+enum ECharacterStance : uint8
+{
+	CrouchingStance,
+	StandingStance,
+};
 /**
  * 
  */
@@ -24,5 +31,73 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient)
 	TObjectPtr<UCharacterTrajectoryComponent> CharacterTrajectory;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Velocity
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Falling
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Velocity", Transient)
+	FVector VelocityXY = FVector::Zero();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Velocity", Transient)
+	FVector PreviousVelocityXY = FVector::Zero();
+
+	UFUNCTION()
+	void RefreshVelocity();
+#pragma endregion
+	
+// ---------------------------------------------------------------------------------------------------------------------
+// Falling
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Falling
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Falling", Transient)
+	float FallStart = 0.f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Falling", Transient)
+	float FallDuration = 0.f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Falling", Transient)
+	uint8 bFalling: 1 = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Falling", Transient)
+	uint8 bPreviousFalling: 1 = 0;
+
+	UFUNCTION()
+	void RefreshFalling();
+#pragma endregion
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Stance (Postures on Land)
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Stance
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Stance", Transient)
+	TEnumAsByte<ECharacterStance> Stance = StandingStance;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Stance", Transient)
+	TEnumAsByte<ECharacterStance> PreviousStance = StandingStance;
+
+	UFUNCTION()
+	void RefreshStance();
+#pragma endregion
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Movement Mode (Different Environments)
+// ---------------------------------------------------------------------------------------------------------------------
+#pragma region Movement Mode
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Swimming", Transient)
+	uint8 bSwimming: 1 = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Swimming", Transient)
+	uint8 bPreviousSwimming: 1 = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Swimming", Transient)
+	uint8 bUnderwater: 1 = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State|Swimming", Transient)
+	uint8 bPreviousUnderwater: 1 = 0;
+	
+	UFUNCTION()
+	void RefreshSwimming();
+#pragma endregion
 };
 
